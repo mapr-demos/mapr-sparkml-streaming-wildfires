82347efa40eb0a7d4d7b2ed95c17e1498132d65c
iandow <kayaking@gmail.com>
2019-02-13 14:53:26 -0800

    specify kafka version by wildcard

--- ml_input_stream.sh
+++ ml_input_stream.sh
@@ -3,10 +3,10 @@
 # USAGE: websocketd --port=3433 --dir=. --devconsole; open http://10.1.1.15:3433/ml_input_stream.sh
 # AUTHOR: Ian Downard
 
-/opt/mapr/kafka/kafka-1.0.1/bin/kafka-console-consumer.sh --topic /user/mapr/ml_input:requester001 --bootstrap-server this.will.be.ignored:9092 &
+/opt/mapr/kafka/kafka-*/bin/kafka-console-consumer.sh --topic /user/mapr/ml_input:requester001 --bootstrap-server this.will.be.ignored:9092 &
 while true; do
 while read -t 1 LINE; do
-echo $LINE | /opt/mapr/kafka/kafka-1.0.1/bin/kafka-console-producer.sh --topic /user/mapr/ml_input:requester001 --broker-list this.will.be.ignored:9092
+echo $LINE | /opt/mapr/kafka/kafka-*/bin/kafka-console-producer.sh --topic /user/mapr/ml_input:requester001 --broker-list this.will.be.ignored:9092
 done
 
 done
